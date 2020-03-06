3033e68fe78f1527491e160e35435f51d7877666
Yang Chen <ychescale9@gmail.com>
2020-03-06 18:50:34 +1100

    Update to Kotlin 1.3.70, kotlinx.serialization 0.20.0, retrofit 2.7.2.

--- src/test/java/com/jakewharton/retrofit2/converter/kotlinx/serialization/KotlinSerializationConverterFactoryStringTest.kt
+++ src/test/java/com/jakewharton/retrofit2/converter/kotlinx/serialization/KotlinSerializationConverterFactoryStringTest.kt
@@ -1,6 +1,7 @@
 package com.jakewharton.retrofit2.converter.kotlinx.serialization
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
 import okhttp3.mockwebserver.MockResponse
@@ -28,6 +29,7 @@ class KotlinSerializationConverterFactoryStringTest {
   @Serializable
   data class User(val name: String)
 
+  @UnstableDefault
   @Before fun setUp() {
     val contentType = MediaType.get("application/json; charset=utf-8")
     val retrofit = Retrofit.Builder()
